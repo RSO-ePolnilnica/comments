0c6880657756184a0ee6e9e8be645f4e99881ddd
Me <vendramin.mitja@gmail.com>
2021-12-29 23:00:20 +0100

    config server

--- api/src/main/java/si/fri/rso/comments/MainController.java
+++ api/src/main/java/si/fri/rso/comments/MainController.java
@@ -1,6 +1,9 @@
 package si.fri.rso.comments;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +12,11 @@ import si.fri.rso.comments.services.CommentService;
 
 @RestController // This means that this class is a Controller
 //@RequestMapping(path="/comments") // This means URL's start with /demo (after Application path)
+@RefreshScope
 public class MainController {
+
+    @Value("${allowCommenting:true}")
+    private boolean canComment;
 
     @Autowired
     private CommentService commentService;
@@ -20,16 +27,19 @@ public class MainController {
     }
 
     @PostMapping(path="/comments/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam Integer stID, @RequestParam Integer usID, @RequestParam String comment) {
+    public @ResponseBody ResponseEntity addNewUser (@RequestParam Integer stID, @RequestParam Integer usID, @RequestParam String comment) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
+        if(canComment == false){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         Comment n = new Comment();
         n.setUserID(usID);
         n.setStationID(stID);
         n.setComment(comment);
         commentService.addComment(n);
-        return "Saved";
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping(path="/comments/all")
